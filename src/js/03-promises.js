dae0dfaa8bf78388a07f925fedb4db4277d4b721
Kris-tina30 <113352879+Kris-tina30@users.noreply.github.com>
2023-03-01 22:12:02 +0300

    виправлення за ментором

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,21 +3,26 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector('.form');
 const firstDelay = document.querySelector('[name="delay"]');
 const delayStep = document.querySelector('[name="step"]');
-const amount = document.querySelector('input[name="amount"]');
+const amountInput = document.querySelector('input[name="amount"]');
 
 form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(event) {
   event.preventDefault();
 
-  for (let i = 0; i + 1 <= Number(amount.value); i += 1) {
-    createPromise(i + 1, Number(firstDelay.value) + Number(delayStep.value) * i)
+  let delay = Number(firstDelay.value);
+  let step = Number(delayStep.value);
+  let amount = Number(amountInput.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
+    delay += step;
   }
 }
 
